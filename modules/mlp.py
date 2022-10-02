9876df1a7d54856a07097abfdb167cfc687a2df8
simplify <zhengtianlun23@gamil.com>
2022-10-02 22:02:31 +0800

    add mlp_vip mlp_cyclemlp for gmlp

---
+++ modules/mlp.py
@@ -0,0 +1,248 @@
+import torch
+import torch.nn as nn
+
+from timm.models.layers import DropPath
+
+import math
+from torch import Tensor
+from torch.nn import init
+from torch.nn.modules.utils import _pair
+from torchvision.ops.deform_conv import deform_conv2d as deform_conv2d_tv
+
+# def _cfg(url='', **kwargs):
+#     return {
+#         'url': url,
+#         'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
+#         'crop_pct': .96, 'interpolation': 'bicubic',
+#         'mean': IMAGENET_DEFAULT_MEAN, 'std': IMAGENET_DEFAULT_STD, 'classifier': 'head',
+#         **kwargs
+#     }
+#
+# default_cfgs = {
+#     'cycle_S': _cfg(crop_pct=0.9),
+#     'cycle_M': _cfg(crop_pct=0.9),
+#     'cycle_L': _cfg(crop_pct=0.875),
+# }
+
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class CycleFC(nn.Module):
+    """
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size,  # re-defined kernel_size, represent the spatial area of staircase FC
+        stride: int = 1,
+        padding: int = 0,
+        dilation: int = 1,
+        groups: int = 1,
+        bias: bool = True,
+    ):
+        super(CycleFC, self).__init__()
+
+        if in_channels % groups != 0:
+            raise ValueError('in_channels must be divisible by groups')
+        if out_channels % groups != 0:
+            raise ValueError('out_channels must be divisible by groups')
+        if stride != 1:
+            raise ValueError('stride must be 1')
+        if padding != 0:
+            raise ValueError('padding must be 0')
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = _pair(stride)
+        self.padding = _pair(padding)
+        self.dilation = _pair(dilation)
+        self.groups = groups
+
+        self.weight = nn.Parameter(torch.empty(out_channels, in_channels // groups, 1, 1))  # kernel size == 1
+
+        if bias:
+            self.bias = nn.Parameter(torch.empty(out_channels))
+        else:
+            self.register_parameter('bias', None)
+        self.register_buffer('offset', self.gen_offset())
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(self.bias, -bound, bound)
+
+    def gen_offset(self):
+        """
+        offset (Tensor[batch_size, 2 * offset_groups * kernel_height * kernel_width,
+            out_height, out_width]): offsets to be applied for each position in the
+            convolution kernel.
+        """
+        offset = torch.empty(1, self.in_channels*2, 1, 1)
+        start_idx = (self.kernel_size[0] * self.kernel_size[1]) // 2
+        assert self.kernel_size[0] == 1 or self.kernel_size[1] == 1, self.kernel_size
+        for i in range(self.in_channels):
+            if self.kernel_size[0] == 1:
+                offset[0, 2 * i + 0, 0, 0] = 0
+                offset[0, 2 * i + 1, 0, 0] = (i + start_idx) % self.kernel_size[1] - (self.kernel_size[1] // 2)
+            else:
+                offset[0, 2 * i + 0, 0, 0] = (i + start_idx) % self.kernel_size[0] - (self.kernel_size[0] // 2)
+                offset[0, 2 * i + 1, 0, 0] = 0
+        return offset
+
+    def forward(self, input: Tensor) -> Tensor:
+        """
+        Args:
+            input (Tensor[batch_size, in_channels, in_height, in_width]): input tensor
+        """
+        B, C, H, W = input.size()
+        return deform_conv2d_tv(input, self.offset.expand(B, -1, H, W), self.weight, self.bias, stride=self.stride,
+                                padding=self.padding, dilation=self.dilation)
+
+    def extra_repr(self) -> str:
+        s = self.__class__.__name__ + '('
+        s += '{in_channels}'
+        s += ', {out_channels}'
+        s += ', kernel_size={kernel_size}'
+        s += ', stride={stride}'
+        s += ', padding={padding}' if self.padding != (0, 0) else ''
+        s += ', dilation={dilation}' if self.dilation != (1, 1) else ''
+        s += ', groups={groups}' if self.groups != 1 else ''
+        s += ', bias=False' if self.bias is None else ''
+        s += ')'
+        return s.format(**self.__dict__)
+
+
+class CycleMLP(nn.Module):
+    def __init__(self, dim, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.mlp_c = nn.Linear(dim, dim, bias=qkv_bias)
+
+        self.sfc_h = CycleFC(dim, dim, (1, 3), 1, 0)
+        self.sfc_w = CycleFC(dim, dim, (3, 1), 1, 0)
+
+        self.reweight = Mlp(dim, dim // 4, dim * 3)
+
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, H, W, C = x.shape
+        h = self.sfc_h(x.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
+        w = self.sfc_w(x.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
+        c = self.mlp_c(x)
+
+        a = (h + w + c).permute(0, 3, 1, 2).flatten(2).mean(2)
+        a = self.reweight(a).reshape(B, C, 3).permute(2, 0, 1).softmax(dim=0).unsqueeze(2).unsqueeze(2)
+
+        x = h * a[0] + w * a[1] + c * a[2]
+
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x
+
+
+class CycleBlock(nn.Module):
+
+    def __init__(self, dim, mlp_hidden_dim, qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, skip_lam=1.0, mlp_fn=CycleMLP):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = mlp_fn(dim, qkv_bias=qkv_bias, qk_scale=None, attn_drop=attn_drop)
+
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        self.norm2 = norm_layer(dim)
+        # mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer)
+        self.skip_lam = skip_lam
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x))) / self.skip_lam
+        x = x + self.drop_path(self.mlp(self.norm2(x))) / self.skip_lam
+        return x
+
+
+class WeightedPermuteMLP(nn.Module):
+    def __init__(self, dim, segment_dim=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.segment_dim = segment_dim
+
+        self.mlp_c = nn.Linear(dim, dim, bias=qkv_bias)
+        self.mlp_h = nn.Linear(dim, dim, bias=qkv_bias)
+        self.mlp_w = nn.Linear(dim, dim, bias=qkv_bias)
+
+        self.reweight = Mlp(dim, dim // 4, dim * 3)
+
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, H, W, C = x.shape
+
+        S = C // self.segment_dim
+        h = x.reshape(B, H, W, self.segment_dim, S).permute(0, 3, 2, 1, 4).reshape(B, self.segment_dim, W, H * S)
+        h = self.mlp_h(h).reshape(B, self.segment_dim, W, H, S).permute(0, 3, 2, 1, 4).reshape(B, H, W, C)
+
+        w = x.reshape(B, H, W, self.segment_dim, S).permute(0, 1, 3, 2, 4).reshape(B, H, self.segment_dim, W * S)
+        w = self.mlp_w(w).reshape(B, H, self.segment_dim, W, S).permute(0, 1, 3, 2, 4).reshape(B, H, W, C)
+
+        c = self.mlp_c(x)
+
+        a = (h + w + c).permute(0, 3, 1, 2).flatten(2).mean(2)
+        a = self.reweight(a).reshape(B, C, 3).permute(2, 0, 1).softmax(dim=0).unsqueeze(2).unsqueeze(2)
+
+        x = h * a[0] + w * a[1] + c * a[2]
+
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x
+
+
+class PermutatorBlock(nn.Module):
+
+    def __init__(self, dim, mlp_hidden_dim, segment_dim=8, qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, skip_lam=1.0, mlp_fn=WeightedPermuteMLP):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = mlp_fn(dim, segment_dim=segment_dim, qkv_bias=qkv_bias, qk_scale=None, attn_drop=attn_drop)
+
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        self.norm2 = norm_layer(dim)
+        # mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer)
+        self.skip_lam = skip_lam
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x))) / self.skip_lam
+        x = x + self.drop_path(self.mlp(self.norm2(x))) / self.skip_lam
+        return x
--- modules/model.py
+++ modules/model.py
@@ -3,7 +3,8 @@ import copy
 import torch
 import torch.nn as nn
 
-from modules.cbam import GatingMlpBlock
+from modules.block import GatingMlpBlock
+from modules.mlp import PermutatorBlock
 from modules.transformation import TPS_SpatialTransformerNetwork
 from modules.feature_extraction import (
     VGG_FeatureExtractor,
@@ -417,7 +418,8 @@ class Ensemble(nn.Module):
         # elif is_train == False:
         #     features, index = self.cross_test(image)
         else:
-            features,index = self.cross_forwardv2(image)
+            # features,index = self.cross_forwardv2(image)
+            features, index = self.cross_forward_dim3(image)
         # out=self.fc(features) #{logics: self.fc(features)}
         out = dict({"logits":features,"index":index,"aux_logits":None})
 
@@ -537,6 +539,36 @@ class Ensemble(nn.Module):
         output = (normal_feat.permute(3,1,2,0).contiguous() * route_info).permute(1,2,0,3).contiguous()
 
         return torch.sum(output,-1),index
+
+    def cross_forward_dim3(self, image, text=None, is_train=True, SelfSL_layer=False):
+        """Transformation stage"""
+        features = [convnet(image) for convnet in self.model]
+        route_info = torch.stack([feature["feature"] for feature in features], 1)
+        route_info = self.mlp3d(route_info)
+        route_info = self.channel_route(route_info).permute(0, 2, 1)
+        # route_info = torch.cat([torch.max(feature,-1)[0] for feature in features],-1)
+        index = self.route(route_info.contiguous())
+        index = self.softargmax1d(torch.squeeze(index, -1))
+        # index [B,I]
+        # index = torch.max(torch.squeeze(index,-1),-1)[1]
+        # index = torch.mean(torch.squeeze(index, -1), -1)
+        # index = torch.squeeze(index,-1)
+
+        # feature_array = torch.stack(features, 1)
+        features = [feature["predict"] for feature in features]
+        B, T, C = features[-1].size()
+        list_len = len(features)
+        normal_feat = []
+        for i in range(list_len - 1):
+            feat = self.pad_zeros_features(features[i], total=C)
+            normal_feat.append(feat)
+        normal_feat.append(features[-1])
+        normal_feat = torch.stack(normal_feat, 0)
+        # normal_feat [I,B,T,C] -> [T,C,B,I] -> [B,T,C,I]
+        output = (normal_feat.permute(2, 3, 1, 0) * index).permute(2, 0, 1, 3).contiguous()
+
+        return torch.sum(output, -1), index
+
     def build_fc(self, hidden_size, nb_classes,device=None):
         self.update_fc(hidden_size, nb_classes,device=None)
 
@@ -555,6 +587,11 @@ class Ensemble(nn.Module):
             GatingMlpBlock(self.feature_dim,self.feature_dim//len(self.model),self.patch),
             GatingMlpBlock(self.feature_dim, self.feature_dim // len(self.model), self.patch),
             GatingMlpBlock(self.feature_dim, self.feature_dim // len(self.model), self.patch),
+        )
+        self.mlp3d = nn.Sequential(
+            PermutatorBlock(self.feature_dim, self.feature_dim // len(self.model)),
+            PermutatorBlock(self.feature_dim, self.feature_dim // len(self.model)),
+            PermutatorBlock(self.feature_dim, self.feature_dim // len(self.model)),
         )
         # [b, num_steps * len] -> [b, len]
         # if self.fc is not None:
@@ -599,7 +636,7 @@ class Ensemble(nn.Module):
     def db_function(self, x, k = 50):
         return torch.reciprocal(1 + torch.exp(-k * x))
 
-    def softargmax1d(self,input, beta=10):
+    def softargmax1d(self,input, beta=5):
         # *_, n = input.shape
         # input = nn.functional.softmax(beta * input, dim=-1)
         # indices = torch.linspace(0, 1, n).to(input.device)
